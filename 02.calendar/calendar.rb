0793c122e5aeb3c22e7664bb5e666e311775e8c8
maimux2x <m0smile10often@gmail.com>
2022-05-20 21:53:57 +0900

    PRレビューいただいた内容の修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,37 +1,27 @@
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
 
-class Calender  
+class Calender
   def initialize(year: Date.today.year, month: Date.today.month)
     @year = year
     @month = month
   end
-  
+
   def calender
-    start_month = Date.new(@year, @month, 1)  
+    start_month = Date.new(@year, @month, 1)
     end_month = Date.new(@year, @month, -1)
-    
+
     puts "#{@month}月  #{@year}".center(20)
-    
-    days = ["日", "月", "火", "水", "木", "金", "土"]
-    days.each do |day|
-      print day.rjust(2)
-    end
 
-    print "\n"
+    puts " 日 月 火 水 木 金 土"
 
-    blank = start_month.wday
-    (blank*3).times {print " "}
-    
-    (start_month..end_month).each do |d|
-      print sprintf("%#3d", d.day)
-      if d.saturday?
-        print "\n"
-      end
+    (start_month.wday * 3).times { print " " }
 
-      if d == end_month
-        break
-      end
+    (start_month..end_month).each do |date|
+      print format("%#3d", date.day)
+      print "\n" if date.saturday?
     end
   end
 end
@@ -41,5 +31,4 @@ cal = Calender.new(
   year: params["y"]&.to_i || Date.today.year,
   month: params["m"]&.to_i || Date.today.month
 )
-puts cal.calender
-
+cal.calender
