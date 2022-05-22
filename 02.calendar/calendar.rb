e3bfea3a79328c954dffe48ab9ad8a9e2a1086b1
maimux2x <m0smile10often@gmail.com>
2022-05-22 16:51:59 +0900

    出力したカレンダーがcalコマンドの見た目と異なっていたため、統一されるように修正
    カレンダーの日付が１桁か２桁かで条件分岐させ、calコマンドと見た目を統一

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -10,20 +10,25 @@ class Calender
   end
 
   def calender
+    puts "#{@month}月 #{@year}".center(20)
+    
+    puts "日 月 火 水 木 金 土"
+    
     start_month = Date.new(@year, @month, 1)
     end_month = Date.new(@year, @month, -1)
-
-    puts "#{@month}月  #{@year}".center(20)
-
-    puts " 日 月 火 水 木 金 土"
-
-    (start_month.wday * 3).times { print " " }
+    
+    print " " * start_month.wday * 3
 
     (start_month..end_month).each do |date|
-      print format("%#3d", date.day)
+      if date.day <= 9
+        print format("%# 2d", date.day)
+        print " " unless date.saturday?
+      else
+        print format("%#-3d", date.day)
+      end
       print "\n" if date.saturday?
-      puts "\n" if date == end_month
     end
+    puts "\n"
   end
 end
 
