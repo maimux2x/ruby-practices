71280513ff4e1c988196e2fda9159ce32d00c95a
maimux2x <m0smile10often@gmail.com>
2022-05-23 21:44:00 +0900

    カレンダーの右詰で出力する処理の箇所を修正
    カレンダーの日付を右詰で出力する箇所を条件分岐を使わず、1行に。他、引数の二重定義とスペルミスの修正。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,13 +3,13 @@
 require 'date'
 require 'optparse'
 
-class Calender
-  def initialize(year: Date.today.year, month: Date.today.month)
+class Calendar
+  def initialize(year:, month:)
     @year = year
     @month = month
   end
 
-  def calender
+  def calendar
     puts "#{@month}月 #{@year}".center(20)
     
     puts "日 月 火 水 木 金 土"
@@ -20,12 +20,7 @@ class Calender
     print " " * start_month.wday * 3
 
     (start_month..end_month).each do |date|
-      if date.day <= 9
-        print format("%# 2d", date.day)
-        print " " unless date.saturday?
-      else
-        print format("%#-3d", date.day)
-      end
+      print date.day.to_s.rjust(2) + " "
       print "\n" if date.saturday?
     end
     puts "\n"
@@ -33,8 +28,8 @@ class Calender
 end
 
 params = ARGV.getopts("y:", "m:")
-cal = Calender.new(
+cal = Calendar.new(
   year: params["y"]&.to_i || Date.today.year,
   month: params["m"]&.to_i || Date.today.month
 )
-cal.calender
+cal.calendar
