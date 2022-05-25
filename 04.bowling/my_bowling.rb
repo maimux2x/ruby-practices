a9c0032e3cdeb28c4b543dbda8519b432dc04944
maimux2x <m0smile10often@gmail.com>
2022-05-25 22:18:47 +0900

    10投目より前にストライクが連続した場合の考慮漏れを修正
    10投目より前にストライクが連続した場合の足し上げが漏れていたため計算の条件を修正した

--- 04.bowling/my_bowling.rb
+++ 04.bowling/my_bowling.rb
@@ -23,7 +23,12 @@ point = 0
 frames.each_with_index do |frame, index|
   point +=
     if frame[0] == 10 && index < 9 # strike
-      10 + frames[index + 1][0] + frames[index + 1][1]
+      # 10投目より前にストライクが連続した場合を考慮
+      if frames[index + 1][0] == 10 && index < 8
+        10 + 10 + frames[index + 2][0]
+      else
+        10 + frames[index + 1][0] + frames[index + 1][1]
+      end
     elsif frame.sum == 10 && index < 9 # spare
       10 + frames[index + 1][0]
     else
