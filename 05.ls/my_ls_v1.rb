883b900960b3544aea58b4ed71c0e307c52527f6
maimux2x <m0smile10often@gmail.com>
2022-06-09 08:43:22 +0900

    レビューいただいた箇所の修正
    クラス名、メソッド名の修正。クラス構文内のメソッドをインスタンスメソッドに変更。

--- 05.ls/my_ls_v1.rb
+++ 05.ls/my_ls_v1.rb
@@ -1,46 +1,46 @@
 # frozen_string_literal: true
 
-require_relative 'add_option'
+require_relative 'app_option'
 
 class LS
-  class << self
-    def current_empty?
-      Dir.empty?(Dir.getwd)
-    end
+  def current_empty?
+    Dir.empty?(Dir.getwd)
+  end
 
-    def create_ary
-      return true if current_empty?
+  def prepare_output
+    return if current_empty?
 
-      option = AddOption.new
-      @first_ary =
-        if option.has?('a')
-          Dir.glob('*', File::FNM_DOTMATCH).sort
-        else
-          Dir.glob('*').sort
-        end
-
-      n = 3
-      splite_ary = Rational(@first_ary.size, n).ceil
-
-      second_ary = @first_ary.each_slice(splite_ary).to_a
-
-      max_size = second_ary.map(&:size).max
-      third_ary = second_ary.map { |ary| ary.values_at(0...max_size) }
-
-      @result = third_ary.transpose
-    end
-
-    def output_current
-      create_ary
-      num = @first_ary.map(&:size).max
-      @result.each do |ary|
-        ary.each do |str|
-          print str&.ljust(num + 5)
-        end
-        puts "\n"
+    option = AppOption.new
+    @first_ary =
+      if option.has?('a')
+        Dir.glob('*', File::FNM_DOTMATCH).sort
+      else
+        Dir.glob('*').sort
       end
+
+    n = 3
+    splite_ary = Rational(@first_ary.size, n).ceil
+
+    second_ary = @first_ary.each_slice(splite_ary).to_a
+
+    max_size = second_ary.map(&:size).max
+    third_ary = second_ary.map { |ary| ary.values_at(0...max_size) }
+
+    third_ary.transpose
+  end
+
+  def output_current
+    result = prepare_output
+    num = @first_ary.map(&:size).max
+
+    result.each do |ary|
+      ary.each do |str|
+        print str&.ljust(num + 5)
+      end
+      puts "\n"
     end
   end
 end
 
-LS.output_current
+ls = LS.new
+ls.output_current
