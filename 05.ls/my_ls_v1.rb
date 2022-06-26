892dd5b9b930372147a11bed44d8a95f28baf6c1
maimux2x <m0smile10often@gmail.com>
2022-06-26 22:55:53 +0900

    レビューいただいた内容の修正2
    変数名の修正、convert_file_typeメソッドの修正

--- 05.ls/my_ls_v1.rb
+++ 05.ls/my_ls_v1.rb
@@ -8,42 +8,41 @@ class LS
     Dir.empty?(Dir.getwd)
   end
 
-  def create_file_name_ary(option)
-    file_name_ary = Dir.glob('*', option.has?('a') ? File::FNM_DOTMATCH : 0).sort
-    file_name_ary = file_name_ary.reverse if option.has?('r')
-    file_name_ary
+  def get_file_names(option)
+    file_names = Dir.glob('*', option.has?('a') ? File::FNM_DOTMATCH : 0).sort
+    file_names = file_names.reverse if option.has?('r')
+    file_names
   end
 
   # オプションなし、aオプション、rオプションの出力情報
-  def prepare_output_file_name(check_option_results)
-    first_ary = check_option_results
+  def prepare_output_file_names(file_names)
+    first_file_names = file_names
     n = 3
-    splite_ary = Rational(first_ary.size, n).ceil
+    splite_num = Rational(first_file_names.size, n).ceil
 
-    second_ary = first_ary.each_slice(splite_ary).to_a
+    second_file_names = first_file_names.each_slice(splite_num).to_a
 
-    max_size = second_ary.map(&:size).max
-    third_ary = second_ary.map { |ary| ary.values_at(0...max_size) }
+    max_size = second_file_names.map(&:size).max
+    third_file_names = second_file_names.map { |name| name.values_at(0...max_size) }
 
-    third_ary.transpose
+    third_file_names.transpose
   end
 
   # lオプションの出力情報
-  def create_file_desc(file_names)
+  def get_file_desc(file_names)
     desc_materials = file_names
 
     desc_materials.map { |desc| File::Stat.new(desc.to_s) }
   end
 
-  def output_file_desc(option)
-    file_names = create_file_name_ary(option)
-    desc_results = create_file_desc(file_names)
+  def output_file_desc(file_names)
+    desc_results = get_file_desc(file_names)
 
     total_blocks = desc_results.map(&:blocks)
     puts "total #{total_blocks.sum}"
 
     desc_results.each_with_index do |desc, index|
-      convert_file_type(desc.ftype)
+      convert_file_type(desc.ftype.to_sym)
       convert_permission(desc.mode.to_s(8))
       print [
         desc.nlink,
@@ -62,17 +61,17 @@ class LS
     return if current_empty?
 
     option = AppOption.new
-    
-    if option.has?('l') #オプション複数指定は次回対応
-      output_file_desc(option)
+
+    file_names = get_file_names(option)
+    if option.has?('l') # オプション複数指定は次回対応
+      output_file_desc(file_names)
     else
-      check_option_results = create_file_name_ary(option)
-      results = prepare_output_file_name(check_option_results)
-      num = check_option_results.map(&:size).max
+      results = prepare_output_file_names(file_names)
+      num = file_names.map(&:size).max
 
       results.each do |ary|
-        ary.each do |str|
-          print str&.ljust(num + 5)
+        ary.each do |name|
+          print name&.ljust(num + 5)
         end
         puts "\n"
       end
@@ -83,18 +82,16 @@ class LS
 
   def convert_file_type(type)
     file_types = {
-      'fifo' => 'p',
-      'characterSpecial' => 'c',
-      'directory' => 'd',
-      'blockSpecial' => 'b',
-      'file' => '-',
-      'link' => 'l',
-      'socket' => 's'
+      fifo: 'p',
+      characterSpecial: 'c',
+      directory: 'd',
+      blockSpecial: 'b',
+      file: '-',
+      link: 'l',
+      socket: 's'
     }
 
-    file_types.each do |key, value|
-      print value if type == key
-    end
+    file_types.filter { |key, _| key == type }.each { |_, value| print value }
   end
 
   def convert_permission(mode)
